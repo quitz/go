f902c0d74f868d8483be9076fe5ce5b63b4347f5
Ning Cui <ningcui8@gmail.com>
2021-04-11 02:08:53 -0400

    Create non-blocking-channel-read.go

---
+++ non-blocking-channel-read.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+
+	// Writer (launched in own goroutine)
+	go func() {
+		time.Sleep(time.Duration(500) * time.Millisecond)
+		fmt.Println("Writing value 1515...")
+		// This send statement is a blocking operation for this thread
+		ch <- 1515
+		// Maybe the following won't have time to execute : never mind
+		fmt.Println("Value 1515 written.")
+	}()
+
+	// Reader: in main thread
+	time.Sleep(time.Duration(1000) * time.Millisecond)
+	fmt.Println("Reading first value (if exists)...")
+	// This receive operation is blocking,
+	// and there is a value ready for reading
+	select {
+	case x, ok := <-ch:
+		if ok {
+			fmt.Printf("Value %d was read.\n", x)
+		} else {
+			fmt.Println("Channel closed!")
+		}
+	default:
+		fmt.Println("No value ready, moving on.")
+	}
+
+	fmt.Println("Reading second value (if exists)...")
+	// This receive operation is blocking,
+	// and there is no value to read : BIG DEADLOCK HAZARD
+	select {
+	case x, ok := <-ch:
+		if ok {
+			fmt.Printf("Value %d was read.\n", x)
+		} else {
+			fmt.Println("Channel closed!")
+		}
+	default:
+		fmt.Println("No value ready, moving on.")
+	}
+
+	fmt.Println("The end")
+}
+
+
+//
