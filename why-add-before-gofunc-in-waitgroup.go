12e1c09681360ef726040d7b620d1261810dccfe
Ning Cui <ningcui8@gmail.com>
2021-04-11 15:20:19 -0400

    Create why-add-before-gofunc-in-waitgroup.go

---
+++ why-add-before-gofunc-in-waitgroup.go
@@ -0,0 +1,2 @@
+https://stackoverflow.com/questions/19208725/example-for-sync-waitgroup-correct
+https://stackoverflow.com/questions/53769216/is-it-safe-to-add-to-a-waitgroup-from-multiple-goroutines
